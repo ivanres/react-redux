b64360565e079f982a4a5002d3fb92b068e990ea
Ivan Rodriguez <ivan.rodriguez@jobsity.com>
2017-04-17 16:21:36 -0500

    Added SignUp "page" and route

--- client/index.js
+++ client/index.js
@@ -3,11 +3,13 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import App from './components/App';
 import Greetings from './components/Greetings';
+import SignupPage from './components/signup/SignupPage';
 
 render(
 	<Router>
 		<div>
 			<Route path="/" component={App} />
-			<Route exact component={Greetings} />
+			<Route exact path="/" component={Greetings} />
+			<Route path="/signup" component={SignupPage} />
 		</div>
 	</Router>, document.getElementById('app'));
