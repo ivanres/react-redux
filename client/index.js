5cfd25e0e1d06834abe891ac7fe29739c5cbedee
Ivan Rodriguez <ivan.rodriguez@jobsity.com>
2017-04-17 15:42:37 -0500

    Added NavigationBar and react-router 4.0

--- client/index.js
+++ client/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import App from './components/App';
+import Greetings from './components/Greetings';
 
-render(<App />, document.getElementById('app'));
+render(
+	<Router>
+		<div>
+			<Route path="/" component={App} />
+			<Route exact component={Greetings} />
+		</div>
+	</Router>, document.getElementById('app'));
