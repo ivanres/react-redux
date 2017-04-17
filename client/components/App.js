946cce0df6769e5bf01f62f9b31a103ffa21f8ee
Ivan Rodriguez <ivan.rodriguez@jobsity.com>
2017-04-17 12:20:56 -0500

    Greetings component

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Greetings from './Greetings';
 
 class App extends React.Component {
 	render() {
 		return (
-			<h1>Hello from react! :s</h1>
+			<Greetings />
 		);
 	}
  }
