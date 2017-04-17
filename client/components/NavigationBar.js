9d2f6c590307746364fc998dc92488776f45f9b2
Ivan Rodriguez <ivan.rodriguez@jobsity.com>
2017-04-17 16:35:24 -0500

    Use actual Links

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default () => {
 	return (
 		<nav className="navbar navbar-default">
 			<div className="container-fluid">
 				<div className="navbar-header">
-					<a className="navbar-brand" href="#">Red Dice</a>
+					<Link to="/" className="navbar-brand">Red Dice</Link>
 				</div>
 				<div className="collapse navbar-collapse">
 					<ul className="nav navbar-nav navbar-right">
-						<li><a href="#">Sign up!!!</a></li>
+						<li><Link to="/signup">Sign up!!!</Link></li>
 					</ul>
 				</div>
 			</div>
