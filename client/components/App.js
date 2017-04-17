31a0d1112789887a7c32397be7a2a4cf47a406b2
Ivan Rodriguez <ivan.rodriguez@jobsity.com>
2017-04-17 11:59:59 -0500

    Use a class instead of a function to define App component. However app is not rendered during first load. It needs a file edit for the markup to be rendered. Afterwards hot reload works fine.

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-export default () => {
-	return (
-		<h1>Hello from react. Welp!</h1>
+class App extends React.Component {
+	render() {
+		return (
+			<h1>Hello from react! :s</h1>
 		);
-}
+	}
+ }
+
+ export default App;
+	
