2bf15134d2e73264edd32caa08f22c754dbe300f
Ivan Rodriguez <iragination@gmail.com>
2017-04-18 23:00:42 -0500

    Server side validations and error display in the front end

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import timezones from '../../data/timezones';
+import classnames from 'classnames'
 //import axios from 'axios';
 
 class SignupForm extends React.Component {
@@ -10,7 +11,9 @@ class SignupForm extends React.Component {
 			email: '',
 			password: '',
 			passwordConfirmation: '',
-			timezone: ''
+			timezone: '',
+			errors: {},
+			isLoading: false
 		}
 
 		this.onChange = this.onChange.bind(this)
@@ -23,8 +26,9 @@ class SignupForm extends React.Component {
 		});
 	}
 
-	onSubmit(e){
+	onSubmit(e){ 
 		e.preventDefault();
+		this.setState({errors: {}, isLoading: true});
 		//console.log(this.state);
 		//axios.post('/api/users', { user: this.state });
 		this.props.userSignupRequest(this.state)
@@ -34,12 +38,13 @@ class SignupForm extends React.Component {
 			.catch( 
 				error => {
 					console.log(error)
+					this.setState({errors: error.response.data, isLoading: false})
 					}
 				);
 	}
 
 	render(){
-
+		const { errors, isLoading } = this.state;
 		const options = Object.keys(timezones).map(k =>
 			<option key={timezones[k]} value={timezones[k]}>{k}</option>
 			);
@@ -48,46 +53,54 @@ class SignupForm extends React.Component {
 			<form onSubmit={this.onSubmit}>
 				<h1>Join our community!</h1>
 
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.username})}>
 					<label className="control-label">Username</label>
 					<input type="text" 
 						value={this.state.username} 
 						onChange={this.onChange}
 						name="username" 
-						className="form-control"/>
+						className="form-control"
+					/>
+					{errors.username && <span className="help-block">{errors.username}</span>} 
 				</div>
 
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.email})}>
 					<label className="control-label">Email</label>
 					<input 
 						onChange={this.onChange}
 						value={this.state.email} 
 						type="text" 
 						name="email" 
-						className="form-control"/>
+						className="form-control"
+					/>
+					{errors.email && <span className="help-block">{errors.email}</span>} 
 				</div>
 
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.password})}>
 					<label className="control-label">Password</label>
 					<input 
 						onChange={this.onChange}
 						value={this.state.password} 
 						type="password" 
 						name="password" 
-						className="form-control"/>
+						className="form-control"
+					/>
+					{errors.password && <span className="help-block">{errors.password}</span>} 
 				</div>
 
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.passwordConfirmation})}>
 					<label className="control-label">Password Confirmation</label>
 					<input 
 						onChange={this.onChange}
 						value={this.state.passwordConfirmation} 
 						type="password" 
 						name="passwordConfirmation" 
-						className="form-control"/>
+						className="form-control"
+					/>
+					{errors.passwordConfirmation && <span className="help-block">{errors.passwordConfirmation}</span>} 
 				</div>
 
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.timezone})}>
 					<label className="control-label">Timezone</label>
 					<select 
 						name="timezone"
@@ -98,10 +111,11 @@ class SignupForm extends React.Component {
 						<option value="" disabled>Choose your Timezone</option>
 						{options}
 					</select>
+					{errors.timezone && <span className="help-block">{errors.timezone}</span>} 
 				</div>
 
 				<div className="form-group">
-					<button className="btn btn-primary btn-lg">
+					<button disabled={isLoading} className="btn btn-primary btn-lg">
 						Sign up
 					</button>
 				</div>
--- server/routes/users.js
+++ server/routes/users.js
@@ -8,6 +8,10 @@ function validateInput(data) {
 
 	let errors = {};
 
+	if (Validator.isEmpty(data.username)) {
+		errors.username = 'This field is required';
+	}
+
 	if (Validator.isEmpty(data.email)) {
 		errors.email = 'This field is required';
 	}
