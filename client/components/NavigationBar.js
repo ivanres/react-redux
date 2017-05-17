6c05d5c6d20f42c22c246ba5e7d94191a11e96ed
Ivan Rodriguez <ivan.rodriguez@jobsity.com>
2017-05-17 13:13:34 -0500

    Convert NavigationBar to class component. Connected it to Redux. Conditionally draw navigation links depending of auth state.

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default () => {
-	return (
-		<nav className="navbar navbar-default">
-			<div className="container-fluid">
-				<div className="navbar-header">
-					<Link to="/" className="navbar-brand">Red Dice</Link>
+class NavigationBar extends React.Component {
+	render() {
+		const { isAuthenticated } = this.props.auth;
+
+		const userLinks = (
+			<ul className="nav navbar-nav navbar-right">
+				<li><a href="#">Logout</a></li>
+			</ul>
+			);
+
+		const guestLinks = (
+			<ul className="nav navbar-nav navbar-right">
+				<li><Link to="/signup">Sign up!</Link></li>
+				<li><Link to="/login">Login</Link></li>
+			</ul>
+			);
+
+		return (
+			<nav className="navbar navbar-default">
+				<div className="container-fluid">
+					<div className="navbar-header">
+						<Link to="/" className="navbar-brand">Red Dice</Link>
+					</div>
+					<div className="collapse navbar-collapse">
+						{ isAuthenticated ? userLinks : guestLinks }
+					</div>
 				</div>
-				<div className="collapse navbar-collapse">
-					<ul className="nav navbar-nav navbar-right">
-						<li><Link to="/signup">Sign up!</Link></li>
-						<li><Link to="/login">Login</Link></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		);
+			</nav>
+			);
+	}
 }
+
+NavigationBar.propTypes = {
+	auth: PropTypes.object.isRequired
+}
+
+function mapStateToProps(state) {
+	return {
+		auth: state.auth
+	}
+}
+
+export default connect(mapStateToProps)(NavigationBar);
